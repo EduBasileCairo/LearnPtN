cafc862e3a416994fdbc7667078bde0564b8207f
EduBasileCairo <ebasilecairo@gmail.com>
2025-02-04 09:29:57 +0100

    Inc Tema5Parte8 hasta pag16

--- Tema5Parte8SentenciasCondicionales.py
+++ Tema5Parte8SentenciasCondicionales.py
@@ -61,7 +61,7 @@ else:
 
 #Otro Ejemplo
 d = { "50862634" : 37 , "43394932" : 32} # Creamos diccionario
-texto = input("Introduce un documento de identidad")
+texto = input("Introduce un documento de identidad: ")
 if texto in d: #Comprobamos si esta en el diccionario
     print("La edad de " + texto + " es " + str(d[texto]))
 else:
@@ -141,3 +141,103 @@ else:
 output_file = open(file_name, 'wb')
 pickle.dump(d, output_file)
 output_file.close()
+
+
+# Bucle while
+# Al igual que en otros lenguajes de programación, el bucle while repite un trozo de código iterativamente mientras se cumpla una determinada condición
+
+### a = 0
+### while a<3:
+###     print (a, end=' ') # Acabamos con espacios en lugar de salto de línea
+### a += 1 # Equivalente a: a = a + 1
+### print (a) # Estamos fuera del while
+### print('Hemos salido fuera del while')
+### # 0 1 2 3 Hemos salido fuera del while
+
+# Al comenzar cada iteración la expresión de inicio del while es evaluada. Si la expresión se cumple (devuelve True), se vuelve a entrar en el while. Si no se cumple (devuelve False), el while deja de ejecutarse y pasamos a la siguiente sentencia tras el while.
+
+# BUCLES y las sentencias CONTINUE, BREAK, PASS y los bloques ELSE
+# Sentencias que permiten alterar el flujo natural de los mismos:
+# break: Interrumpe el flujo y sale fuera de bucle.
+# continue: Salta al comienzo de la siguiente iteración del bucle.
+# pass: No hace nada. Es una sentencia vacía.
+# else: Al finalizar un bucle: Se ejecuta sólo si el bucle ha finalizado con normalidad. Es decir, se ejecuta si el bucle finaliza sin haber ejecutado un break.
+
+# Sentencia break:
+a = 5
+while a: # Utilizamos la propia variable como condición
+    print (a, end=' ')
+    if a == 2: 
+        break
+    a -= 1
+print ('\nFuera del Bucle por break.')
+print('Valor de "a": {}'.format(a))
+# 5 4 3 Fuera del Bucle.
+
+#Sentencia continue: permite interrumpir la iteración actual
+a = 7
+while bool(a):
+    a -= 1
+    if a % 2 == 0:
+        continue # Saltamos a la siguiente iteracción si es a es par
+    print (a, end=' ')
+print ('\nFuera del Bucle por continue.')
+# 5 3 1 Fuera del Bucle
+
+# La sentencia pass: no hace absolutamente nada. Es un marcador de posición para dejar bucles vacíos
+#
+### a = 5
+### while a:
+###     pass # Presiona Ctrl-C para abortar la ejecución
+
+# Bloques else al finalizar bucles: tenemos dos maneras de finalizar bucles. La primera es una finalización limpia, sin interrupciones, y la segunda es mediante el uso de la sentencia break. Para saber si bucle ha finalizado de cierta manera, en muchos lenguajes de programación se utilizan flags (banderas) en el código que luego hay que evaluar. En Python, esto no es necesario ya que podemos utilizar bloques else al final de un bucle.
+a = 13
+b = a // 2 # División entera. P. ej. 13 // 2 == 6
+while b > 1:
+    if a % b == 0: # % es el operador resto. P. ej. 10 % 5 == 0
+        print('{b} es factor de {a}'.format(b,a))
+    break
+    b -= 1
+else:
+    print('{} es primo'.format(a))
+print ('\nFuera del Bucle por sentencia else.')
+# 13 es primo Fuera del Bucle.
+
+# El bucle FOR: La sintaxis del bucle for es muy sencilla y parecida a la del bucle while, con la excepción que en la cabecera del for, en lugar de evaluar una expresión en cada iteración, vamos asignando elementos de un iterador a una variable.
+# Ejemplo 1: 
+for s in ['Me', 'gusta', 'Python!']:
+    print(s, end=' ')
+# Me gusta Python!
+
+#Ejemplo 2:
+a = 0
+for x in [1, 2, 3, 4]:
+    a += x
+print(a)
+# Salida: 10
+
+#Ejemplo 3:
+for c in 'Me gusta Python!':
+    print(c, end=' ')
+# M e g u s t a P y t h o n ! Recorre el string y agrega una espacio entre cada letra
+
+#Ejemplo 4:
+for k in d:
+    print(k, end=' ')
+# Apellidos edad nombre.
+# Al pasarle un diccionario a un for, lo recorremos por sus claves. Hay varias maneras de extraer los valores de un diccionario. Más adelante, veremos cómo recorrer simultáneamente las claves y valores, pero de momento veamos una manera muy sencilla de hacerlo:
+keys = ['nombre', 'apellidos', 'edad']
+values = ['Guido', 'van Rossum', '60']
+d = dict(zip(keys, values)) # Creamos el diccionario
+for k in d:
+    info = '{}: {}'.format(k, d[k]) # Texto formateado con claves y valores
+    print(info) # Imprimimos dentro del bucle
+# Apellidos: van Rossum
+# edad: 60
+# nombre: Guido
+
+# Asignación en TUPLAS (página 16)
+
+
+
+
