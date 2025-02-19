ffe684310d5940b2e39500f59a9c05fe68d201b5
EduBasileCairo <ebasilecairo@gmail.com>
2025-02-19 12:23:11 +0100

    Act Tema7Parte4

--- Tema7Parte4Unittest.py
+++ Tema7Parte4Unittest.py
@@ -11,3 +11,8 @@ resultado = area(radio) # Llamamos a la función area y guardamos el resultado
 
 print('El área de la circunferencia es: ', resultado) # Imprimimos el resultado
 
+valores = [1, 3, 5 ]# 0 , -1, -3, 2+3j, True, 'hola']
+
+for v in valores:
+    areaCalculada = area(v)
+print('Para el valor', v, 'el área es', areaCalculada)
