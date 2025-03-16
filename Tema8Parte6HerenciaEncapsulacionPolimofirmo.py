31c0eed64a6d877239521c8e298f8ef31f1e4762
EduBasileCairo <ebasilecairo@gmail.com>
2025-03-16 15:07:02 +0100

    Inc Tema8Parte6Herencia2doEjemplo

---
+++ Tema8Parte6HerenciaEncapsulacionPolimofirmo.py
@@ -0,0 +1,32 @@
+# HERENCIA
+
+# Supongamos que tenemos una clase padre vehículo.Necesitamos crear dos clases hijas, Coche y Moto que heredarán de Vehículo. Posteriormente crearemos una instancia de la clase Coche y otra de la clase Moto.
+
+class Vehiculo():
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = "Negro"
+        self.arrancado = False
+        self.parado = True
+    def arrancar(self):
+        self.arrancado = True
+        self.parado = False
+    def parar(self):
+        self.parado = True
+        self.arrancado = False
+    def resumen(self):
+        print("Marca:", self.marca, "\n", "Modelo:", self.modelo, "\n", "Color:", self.color, "\n", "Está arrancado:", self.arrancado,"\n", "Está parado:", self.parado)
+
+miCoche = Vehiculo("Renault", "Megane")
+
+miCoche.arrancar()
+
+miCoche.resumen()
+
+class Moto(Vehiculo):
+    pass
+
+miMoto = Moto("Kawasaki", "Ninja")
+
+miMoto.resumen()
